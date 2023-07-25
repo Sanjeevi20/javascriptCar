7cca97c8b5c8ac9eace2de8f17f692f88427e79c
Sanjeevi20 <136660773+Sanjeevi20@users.noreply.github.com>
2023-07-25 12:35:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function white(){
-    document.getElementById("imd").setAttribute("src",'./white.jpg')
+    document.getElementById("imd").setAttribute("src",'https://img.gaadicdn.com/images/car-images/930x620/Audi/Audi-R8/2391/1549274593297/Audi-R8_Ibis-white_9c9e91.jpg')
 }
 
 function yellow(){
-    document.getElementById("imd").setAttribute("src",'./yellow.jpg')
+    document.getElementById("imd").setAttribute("src",'https://img.gaadicdn.com/images/car-images/930x620/Audi/Audi-R8/2391/1549274593297/Audi-R8_Vegas-yellow_ae8b31.jpg')
 }
 
 function black(){
-    document.getElementById("imd").setAttribute("src",'./black.jpg')
+    document.getElementById("imd").setAttribute("src",'https://img.gaadicdn.com/images/car-images/930x620/Audi/Audi-R8/2391/1549274593297/Audi-R8_Mythos-black,-metallic_272729.jpg')
 }
 
 const clk1=document.querySelector("#clk");
