7ef43438dc0a4a1a2239a43669bc66b158ebf399
Sanjeevi20 <136660773+Sanjeevi20@users.noreply.github.com>
2023-07-25 12:14:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function white(){
+    document.getElementById("imd").setAttribute("src",'/white.jpg')
+}
+
+function yellow(){
+    document.getElementById("imd").setAttribute("src",'/yellow.jpg')
+}
+
+function black(){
+    document.getElementById("imd").setAttribute("src",'/black.jpg')
+}
+
+const clk1=document.querySelector("#clk");
+clk1.addEventListener("click",function(){
+    clk1.textContent="Added to cart";
+    event.target.style.backgroundColor = 'white';
+    event.target.style.color = 'black';
+})
